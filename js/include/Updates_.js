17d9720b8e50253b7dee9480f4a141bce7335ee0
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2024-03-10 18:57:48 +1030

    added next version section
    shows progress and notes on completion of the next version

--- js/include/Updates_.js
+++ js/include/Updates_.js
@@ -1,11 +1,17 @@
 const UPDATES = {
     "1.0.0": {
         Version: "1.0.0",
+        Description: `<br>v1.1.0 Coming Soon!!<br>Thank you for your patience!<br><br>`,
         UpdateType: "release",
         Notes: [
             { type: "publish", note: "Extension Released!" }
         ],
         Issues: [],
-        PlannedFeatures: []
+        PlannedFeatures: [],
+        NextUpdate: {
+            version: "1.1.0",
+            progress: 85,
+            note: "Finalising and polishing changes"
+        }
     }
 };
--- js/updates.js
+++ js/updates.js
@@ -14,7 +14,34 @@ function updateUpdates() {
     }
 
     const versionOnAddButton = document.querySelector(".main .add_button .version");
-    versionOnAddButton.textContent = "v"+VERSION;
+    versionOnAddButton.textContent = "v" + VERSION;
+    
+    const versionDescriptionDiv = document.querySelector(".main .description");
+    versionDescriptionDiv.innerHTML = currentVersion.Description;
+
+    if (currentVersion.Description || currentVersion.NextUpdate) {
+        document.querySelector(".main .description_seperator").style.display = "block";
+    }
+
+    if (currentVersion.NextUpdate) {
+        document.querySelector(".main .next_version_seperator").style.display = "block";
+        document.querySelector(".main .next_version_info").innerHTML = `
+            <h3>Next Version ${currentVersion.NextUpdate.version ? `(v${currentVersion.NextUpdate.version}) ` : ""}:</h3>
+            
+            <div class="update_progress">
+                <div class="completion_progress">
+                    <div class="bar" style="width:${currentVersion.NextUpdate.progress}%"></div>
+                </div>
+
+                <span class="completion">${currentVersion.NextUpdate.progress}%</span>
+            </div>
+
+            <br>
+            
+            <span class="note">${currentVersion.NextUpdate.note}</span>
+            <br><br>
+        `;
+    }
 
     for (const note of currentVersion.Notes) {
         let noteDiv = document.createElement("div");
