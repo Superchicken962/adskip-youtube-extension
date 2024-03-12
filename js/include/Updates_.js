23461868c4692a493ca252a6881f18469356cf12
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2024-03-13 01:19:49 +1030

    v1.1.0 ready

--- js/include/Updates_.js
+++ js/include/Updates_.js
@@ -1,4 +1,28 @@
 const UPDATES = {
+    "1.1.0": {
+        Date: "12/03/2024",
+        Version: "1.1.0",
+        Description: "<p>The long awaited update!<br><br>Unfortunately, I cannot guarentee that all bugs have been 100% fixed, but I can say that I have applied some fixes which will hopefully fix, or reduce the likelihood of them occuring.</p><p>Please be sure to report any bugs and issues and I will try to fix them! Any bugs I know of will be listed down below!</p>",
+        UpdateType: "major",
+        Notes: [
+            { type: "fix", note: "Fixed playback rate not changing back to normal after speeding up an ad" },
+            { type: "fix", note: "Fixed video previews from playing unmuted" },
+            { type: "fix", note: "Applied fix that will hopefully make ad skipping more reliable" },
+            { type: "addition", note: "Sped up ads should now count as 'skipped ads' and contribute points to your rank" },
+            { type: "addition", note: "Added new visuals below video to indicate ads skipped (toggleable)" },
+            { type: "addition", note: "Added new button next to title to try and fix ads not skipping properly when clicked!" },
+            { type: "addition", note: "Added leaderboard where you can compare your stats to others" },
+            { type: "fix", note: "Properly aligned checkboxes to labels in extension window" },
+            { type: "adjustment", note: "Adjusted amount of skips needed to reach some ranks to account for ads sped up" }
+        ],
+        Issues: [
+            { severity: "0", note: "Ad skipped visual may sometimes show up in weird places or not at all" },
+            { severity: "0", note: "Ad skipping still may not work sometimes, however should be resolved by clicking the new fix button" }
+        ],
+        PlannedFeatures: [
+            { type: "improvement", note: "UI Improvements", reliability: 50 },
+        ]
+    },
     "1.0.0": {
         Version: "1.0.0",
         Description: `<br>v1.1.0 Coming Soon!!<br>Thank you for your patience!<br><br>`,
