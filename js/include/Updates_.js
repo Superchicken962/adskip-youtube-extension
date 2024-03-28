ebdff9693cbeea716752321b5e318cf562bcaf0c
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2024-03-28 19:34:03 +1030

    removed mistake
    NextUpdate not intended to be shown yet

--- js/include/Updates_.js
+++ js/include/Updates_.js
@@ -23,12 +23,7 @@ const UPDATES = {
         ],
         PlannedFeatures: [
             { type: "improvement", note: "UI Improvements", reliability: 50 },
-        ],
-        NextUpdate: {
-            version: "1.1.1",
-            progress: 0,
-            note: "Not started"
-        }
+        ]
     },
     "1.0.0": {
         Version: "1.0.0",
